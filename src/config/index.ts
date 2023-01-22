787f85dc26ad3fce142bc35b9fa594e2cffe6902
Joaquin <222joaignaciorodriguez222>
2023-01-22 17:30:16 -0300

    primer cambio

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,7 @@ dotenv.config({
 });
 
 export default {
-    MONGO_ATLAS_URL: process.env.mongo_atlas || 'mongoSRV', //mongo_atlas='mongodb+srv://admin:admin@coderhouse.dahey8p.mongodb.net/Entrega12?retryWrites=true&w=majority'
-    PORT: process.env.PORT || 8080,
+    MONGO_ATLAS_URL: process.env.mongo_atlas || 'mongoSRV',
     SQL_CONNECTION: {
         client: 'mysql2',
         connection: {
